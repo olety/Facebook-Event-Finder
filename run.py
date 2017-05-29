707a385944b9ba3fbbd5db33e85774a2d9ff24d1
Oleksii Kyrylchuk <olety7@gmail.com>
2017-05-29 22:12:19 +0200

    Fixed a bug in run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import json
 from flask import Flask, request
 
 import fbd
-url = input('DB URL')
+url = input('DB URL: ')
 app = Flask(__name__)
 s = fbd.Storage(url)
 
@@ -17,9 +17,9 @@ def show_places():
                            s.get_places_coords(float(request.args['lat']),
                                                float(request.args['lon']),
                                                distance=float(request.args['dist']))],
-                          default=fbd.tools.default_json_serializer)
+                          default=fbd.Storage.default_json_serializer)
     else:
-        return json.dumps(s.get_all_place_ids(), default=fbd.tools.default_json_serializer)
+        return json.dumps(s.get_all_place_ids(), default=fbd.Storage.default_json_serializer)
 
 
 @app.route('/events', methods=['GET'])
@@ -30,9 +30,9 @@ def show_events():
         return json.dumps([_.to_dict() for _ in s.get_events_coords(request.args['lat'],
                                                                     request.args['lon'],
                                                                     distance=request.args['dist'])],
-                          default=fbd.tools.default_json_serializer)
+                          default=fbd.Storage.default_json_serializer)
     else:
-        return json.dumps(s.get_all_event_ids(), default=fbd.tools.default_json_serializer)
+        return json.dumps(s.get_all_event_ids(), default=fbd.Storage.default_json_serializer)
 
 
 if __name__ == '__main__':
